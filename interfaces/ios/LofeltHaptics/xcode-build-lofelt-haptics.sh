b53b415175e754bd78e7c89a539de167aee627d2
Andrea Mazzini <andreamaz@fb.com>
2024-01-15 14:25:40 +0100

    Add Xcode fix

--- interfaces/ios/LofeltHaptics/xcode-build-lofelt-haptics.sh
+++ interfaces/ios/LofeltHaptics/xcode-build-lofelt-haptics.sh
@@ -8,6 +8,14 @@ export PATH="$HOME/.cargo/bin:$PATH"
 # cd to the workspace root
 cd ../../../
 
+if [[ -n "${DEVELOPER_SDK_DIR:-}" ]]; then
+  # Assume we're in Xcode, which means we're probably cross-compiling.
+  # In this case, we need to add an extra library search path for build scripts and proc-macros,
+  # which run on the host instead of the target.
+  # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
+  export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
+fi
+
 # Use the ios-arm64 toolchain when the ENABLE_BITCODE flag is set.
 # We only need to enable bitcode when building the framework for distribution
 # (see build-universal-framework.sh), otherwise the default toolchain can be used.
